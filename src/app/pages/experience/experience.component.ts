424e030e64256330558f4e330080312976d46fac
Adrian Cabello <c.adriancabello@gmail.com>
2022-04-22 12:20:56 -0300

    add experience

--- src/app/pages/experience/experience.component.ts
+++ src/app/pages/experience/experience.component.ts
@@ -7,6 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperienceComponent implements OnInit {
 
+  experience;
+
+  experiences = [
+    {
+      'id': 1,
+      company: 'Spark Digital',
+      title: 'Senior Frontend Developer',
+      start: 'Sept. 2021',
+      end: 'Currently',
+      info: 'lorem asd awlpd awlf[wp law[epwad apw[dwa p[dlaw[plf gfaw gawdawd',
+      items: [{
+        text: 'Bla bla bla'
+      }, {
+        text: 'Bla bla bla'
+      }, {
+        text: 'Bla bla bla'
+      },]
+    }
+  ]
+
   constructor() { }
 
   ngOnInit(): void {
